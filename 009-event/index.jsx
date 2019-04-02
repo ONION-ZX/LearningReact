e74d1eb6a5d1337630f9deb3f29efc099c06f110
ONION-ZX <zxonion@gmail.com>
2019-04-02 20:30:57 +0800

    event-02-bind

--- 009-event/index.jsx
+++ 009-event/index.jsx
@@ -1,10 +1,14 @@
 class Title extends Component {
-    handleClickOnTitle() {
-        console.log('yo');
+    handleClickOnTitle(e) {
+        console.log(e.target.innerHTML);
     }
     render() {
         return(
-            <h1 onClick="{this.handleClickOnTitle}">React小书</h1>
+            /*    手动bind到当前实例
+            bind: 将事件监听方法中的this绑定到当前组件实例上
+                  渲染列表元素时把列表元素传入事件监听函数中
+            */       
+            <h1 onClick="{this.handleClickOnTitle.bind(this, 'Hello')}">React小书</h1>
         )
     }
 }
