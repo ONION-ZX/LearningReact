bea662723a36219e812bbaa3ab19cd2aaaeca386
ONION-ZX <zxonion@gmail.com>
2019-04-04 11:28:18 +0800

    app 处理用户输入

--- 013-comment-app/src/CommentInput.js
+++ 013-comment-app/src/CommentInput.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
 
 class CommentInput extends Component {
+    constructor() {
+        super();
+        this.state = {
+            username: '',
+            content: ''
+        }
+    }
+    //由于React.js认为所有状态都应该由React.js的state控制，必须要用setState才能更新组件的内容。
+    handleUsernameChange(event) {
+        this.setState({
+            username: event.target.value
+        })
+    }
+    handleContentChange(event) {
+        this.setState({
+            content: event.target.value
+        })
+    }
     render() {
         return(
-            <div>CommentInput</div>
+            <div className='comment-input'>
+                <div className="comment-field">
+                    <span className="comment-field-name">用户名：</span>
+                    <div className="comment-field-input">
+                        <input value={this.state.username} 
+                            onChange={this.handleUsernameChange.bind(this)}
+                        />
+                    </div>
+                </div>
+                <div className="comment-field">
+                    <span className="comment-field-name">评论内容：</span>
+                    <div className="comment-field-input">
+                        <textarea value={this.state.content} 
+                                onChange={this.handleContentChange.bind(this)}
+                        />
+                    </div>
+                </div>
+                <div className="comment-field-button">
+                    <button>发布</button>
+                </div>
+            </div>
         )
     }
 }
