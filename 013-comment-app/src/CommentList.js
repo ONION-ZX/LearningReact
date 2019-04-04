1c8fed44bd135548dcc9f880d099ef010371496b
ONION-ZX <zxonion@gmail.com>
2019-04-04 12:02:19 +0800

    app 测试数据

--- 013-comment-app/src/CommentList.js
+++ 013-comment-app/src/CommentList.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Comment from './Comment'
 
 class CommentList extends Component {
-    render() {
-        return (
-            <div>CommentList</div>
-        )
-    }
+  render() {
+    const comments = [
+      {username: 'Jerry', content: 'Hello'},
+      {username: 'Tomy', content: 'World'},
+      {username: 'Lucy', content: 'Good'}
+    ]
+
+    return (
+      <div>
+        {comments.map((comment, i) => <Comment comment={comment} key={i} />)}
+      </div>
+    )
+  }
 }
 
-export default CommentList;
+export default CommentList
