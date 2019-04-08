e1f032343af038c0ed4209c039e2d1759a02d7eb
ONION-ZX <zxonion@gmail.com>
2019-04-08 18:44:29 +0800

    评论持久化

--- 014-comment-practise/src/CommentApp.js
+++ 014-comment-practise/src/CommentApp.js
@@ -9,14 +9,28 @@ class CommentApp extends Component {
             comments: [],
         }
     }
+    componentWillMount() {
+        this._loadComments();
+    }
+    _loadComments() {
+        let comments = localStorage.getItem('comments');
+        if(comments) {
+            comments = JSON.parse(comments);
+            this.setState({comments})
+        }
+    }
+    _saveComments(comments) {
+        localStorage.setItem('comments', JSON.stringify(comments));
+    }
     onSubmit(comment) {
         if(!comment) return;
         if(!comment.name) return alert('请输入用户名!');
-        if(!comment.content) return alert('请输入评论内容!')
-        this.state.comments.push(comment);
-        this.setState({
-            comments: this.state.comments
-        })
+        if(!comment.content) return alert('请输入评论内容!');
+        const comments = this.state.comments;
+        comments.push(comment);
+        this.setState({comments});
+        //每次提交评论都要把评论列表数据保存一次
+        this._saveComments(comments);
     }
     render() {
         return(
