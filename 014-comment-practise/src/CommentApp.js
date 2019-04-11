76bd8543b778b117d75a45e74b8c00a75f68834a
ONION-ZX <zxonion@gmail.com>
2019-04-12 00:15:12 +0800

    评论功能修改

--- 014-comment-practise/src/CommentApp.js
+++ 014-comment-practise/src/CommentApp.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import CommentInput from './CommentInput';
 import CommentList from './CommentList';
 
 class CommentApp extends Component {
-    constructor() {
-        super();
+    static propTypes = {
+        data: PropTypes.any,
+        saveData: PropTypes.func.isRequired
+    }
+    constructor(props) {
+        super(props);
         this.state = {
-            comments: [],
+            comments: props.data
         }
     }
-    componentWillMount() {
-        this._loadComments();
-    }
-
     _loadComments() {
         let comments = localStorage.getItem('comments');
         if(comments) {
@@ -28,7 +28,7 @@ class CommentApp extends Component {
         const comments = this.state.comments;
         comments.splice(index, 1);
         this.setState({comments});
-        this._saveComments(comments);
+        this.props.saveData(comments);
     }
     handleSubmit(comment) {
         if(!comment) return;
