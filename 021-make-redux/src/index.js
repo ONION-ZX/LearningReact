4bb79f2a68fa7a4312ac034e55e7cab7e7123a44
ONION-ZX <zxonion@gmail.com>
2019-04-16 20:36:23 +0800

    抽离store/监控数据变化

--- 021-make-redux/src/index.js
+++ 021-make-redux/src/index.js
@@ -1,45 +1,58 @@
+function createStore(state, stateChanger) {
+  const listeners = [];
+  const subscribe = (listener) => listener.push(listener);
+  const getState = () => state; //获取state数据
+  const dispatch = (action) => {
+    stateChanger(state, action);
+    listeners.forEach((listener) => listener());
+  }
+  return { getState, dispatch, subscribe};
+}
+
+function renderApp (appState) {
+  renderTitle(appState.title)
+  renderContent(appState.content)
+}
+
+function renderTitle (title) {
+  const titleDOM = document.getElementById('title')
+  titleDOM.innerHTML = title.text
+  titleDOM.style.color = title.color
+}
+
+function renderContent (content) {
+  const contentDOM = document.getElementById('content')
+  contentDOM.innerHTML = content.text
+  contentDOM.style.color = content.color
+}
+
 let appState = {
-    title: {
-      text: 'React.js 小书',
-      color: 'red',
-    },
-    content: {
-      text: 'React.js 小书内容',
-      color: 'blue'
-    }
+  title: {
+    text: 'React.js 小书',
+    color: 'red',
+  },
+  content: {
+    text: 'React.js 小书内容',
+    color: 'blue'
   }
+}
 
-  function dispatch(action) {
-    switch(action.type) {
-      case 'UPDATE_TITLE_TEXT':
-        appState.title.text = action.text;
-        break;
-      case 'UPDATE_TITLE_COLOR':
-        appState.title.color = action.color;
-        break;
-      default:
-        break;
-    }
+function stateChanger (state, action) {
+  switch (action.type) {
+    case 'UPDATE_TITLE_TEXT':
+      state.title.text = action.text
+      break
+    case 'UPDATE_TITLE_COLOR':
+      state.title.color = action.color
+      break
+    default:
+      break
   }
+}
 
-  function renderApp (appState) {
-    renderTitle(appState.title)
-    renderContent(appState.content)
-  }
-  
-  function renderTitle (title) {
-    const titleDOM = document.getElementById('title')
-    titleDOM.innerHTML = title.text
-    titleDOM.style.color = title.color
-  }
-  
-  function renderContent (content) {
-    const contentDOM = document.getElementById('content')
-    contentDOM.innerHTML = content.text
-    contentDOM.style.color = content.color
-  }
+const store = createStore(appState, stateChanger);
+store.subscribe(() => renderApp(store.getState())); //监听数据变化
 
-  renderApp(appState); // 首次渲染页面
-  dispatch({type: 'UPDATE_TITLE_TEXT', text: '《Reacr.js 小书》'});
-  dispatch({type: 'UPDATE_TITLE_COLOR', color: 'blue'});
-  renderApp(appState); // 渲染新数据
+renderApp(store.getState()); //首次渲染
+store.dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》' }) // 修改标题文本
+store.dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'blue' }) // 修改标题颜色
