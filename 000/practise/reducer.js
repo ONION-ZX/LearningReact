7327707e37d7fc78b961f44d4d83802743527f91
ONION-ZX <zxonion@gmail.com>
2019-04-22 23:49:16 +0800

    初始化工程

--- 000/practise/reducer.js
+++ 000/practise/reducer.js
@@ -6,22 +6,21 @@ const ADD_USER = "ADD_USER"
 const DELETE_USER = "DELETE_USER"
 const UPDATE_USER = "UPDATE_USER"
 
-const usersReducer =(state=[],action)=>{
-  switch(action.type){
+const usersReducer = (state=[], action) => {
+  switch(action.type) {
     case ADD_USER:
-      return [...state,action.user]
+      return [...state, action.user]
     case DELETE_USER:
-      return[...state.slice(0,action.index),...state.slice(action.index+1)]
+      return [...state.slice(0, action.index), ...state.slice(action.index+1)]
     case UPDATE_USER:
       return [...state.map((user, index) => {
-        if (index === action.index) {
+        if(index === action.index) {
           return {...user, ...action.user}
         } else {
-          return user 
+          return user
         }
       })]
     default:
       return state
   }
-  
 }
