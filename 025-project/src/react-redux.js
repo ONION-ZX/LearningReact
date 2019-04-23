7e5797ac4449f5efe3b209410c406f33e8b82e85
ONION-ZX <zxonion@gmail.com>
2019-04-23 18:28:29 +0800

    connect重构

---
+++ 025-project/src/react-redux.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export const connect = (mapStateToProps) => (WrappedComponent) => {
+    class Connect extends Component {
+        static contextTypes = {
+            store: PropTypes.object
+        }
+
+        constructor() {
+            super();
+            //保存需要传给被包装组件的所有参数
+            this.state = { allProps: {}};
+        }
+        componentWillMount() {
+            const { store } = this.context;
+            this._updateProps();
+            store.subscribe(() => this._updateProps());
+        }
+
+        _updateProps() {
+            const { store } = this.context;
+            let stateProps = mapStateToProps(store.getState(), this.props);
+            this.setState({
+                allProps: {
+                    ...stateProps,
+                    ...this.props
+                }
+            })
+        }
+
+        render() {
+            return <WrappedComponent {...this.state.allProps} />
+        }
+    }
+    return Connect
+}
