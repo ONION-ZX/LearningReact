daea59a65fe47a97036b43bdbcd5d66cd00623bf
ONION-ZX <zxonion@gmail.com>
2019-05-02 22:38:27 +0800

    与运算符

--- 027-core-concept/conditionalRendering.js
+++ 027-core-concept/conditionalRendering.js
@@ -74,3 +74,25 @@ ReactDOM.render(
     <LoginControl />,
     document.getElementById('root')
 )
+
+/**
+ * &&运算符
+ */
+
+function Mailbox(props) {
+    const unreadMessages = props.unreadMessages;
+    return (
+        <div>
+            <h1>Hello!</h1>
+            {unreadMessages.length > 0 && 
+                <h2>You have {unreadMessages.length} unread messages.</h2>
+            }
+        </div>
+    )
+}
+
+const messages = ['React', 'Re: React', 'Re:Re: React'];
+ReactDOM.render(
+    <Mailbox unreadMessages={messages}/>,
+    document.getElementById('root')
+)
