42b8f4f6966f63acc9cb2f0d439b444067f87de5
ONION-ZX <zxonion@gmail.com>
2019-05-03 16:42:56 +0800

    温度计算器

--- 027-core-concept/stateUp.js
+++ 027-core-concept/stateUp.js
@@ -10,6 +10,25 @@ const scaleNames = {
     f: 'Fahrenheit'
 }
 
+function toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+ /**
+  * 当输入 temperature 的值无效时，函数返回空字符串，反之，则返回保留三位小数并四舍五入后的转换结果
+  * @param {string} temperature 
+  * @param {function} convert 
+  */
+function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if(Number.isNaN(input)) return '';
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+}
 class TemperatureInput extends React.Component {
     constructor(props) {
         super(props);
@@ -17,10 +36,10 @@ class TemperatureInput extends React.Component {
         this.handleChange = this.handleChange.bind(this);
     }
     handleChange(e) {
-        this.setState({temperature: e.target.value});
+        this.props.onTemperatureChange(e.target.value);
     }
     render() {
-        const temperature = this.state.temperature;
+        const temperature = this.props.temperature;
         const scale = this.props.scale;
 
         return (
@@ -33,12 +52,52 @@ class TemperatureInput extends React.Component {
 }
 
 class Calculator extends React.Component {
-    render() {
-        return (
-            <div>
-                < TemperatureInput scale="c" />
-                < TemperatureInput scale="f" />
-            </div>
-        )
-    }
+   constructor(props) {
+       super(props);
+       this.state = {
+           temperature: '',
+           scale: 'c'
+       };
+       this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+       this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+   }
+   handleCelsiusChange(temperature) {
+       this.setState({
+           scale: 'c',
+           temperature: temperature
+       });
+   }
+   handleFahrenheitChange(temperature) {
+       this.setState({
+           scale: 'f',
+           temperature: temperature
+       });
+   }
+   render() {
+       const scale = this.state.scale;
+       const temperature = this.state.temperature;
+       const celsius = scale === 'f' ? tryConvert(temperature) : temperature;
+       const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+
+       return(
+           <div>
+               <TemperatureInput 
+                   scale="c"
+                   temperature={celsius}
+                   onTemperatureChange={this.handleCelsiusChange}
+               />
+
+               <TemperatureInput 
+                   scale="f"
+                   temperature={fahrenheit}
+                   onTemperatureChange={this.handleFahrenheitChange}
+               />
+
+               <BoilingVerdict
+                   celsius={parseFloat(celsius)}
+               />
+           </div>
+       )
+   }
+
 }
