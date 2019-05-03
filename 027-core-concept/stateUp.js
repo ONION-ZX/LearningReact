196aa3d997b6b21bd1b80b59f691bcd846c6654b
ONION-ZX <zxonion@gmail.com>
2019-05-03 15:59:07 +0800

    添加第二个输入框

--- 027-core-concept/stateUp.js
+++ 027-core-concept/stateUp.js
@@ -5,26 +5,40 @@ function BoilingVerdict(props) {
     return <p>The water would not boil.</p>
 }
 
-class Calculator extends React.Component {
+const scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+}
+
+class TemperatureInput extends React.Component {
     constructor(props) {
         super(props);
         this.state = {temperature: ''};
         this.handleChange = this.handleChange.bind(this);
     }
     handleChange(e) {
-        this.setState({temperature:e.target.value});
+        this.setState({temperature: e.target.value});
     }
     render() {
         const temperature = this.state.temperature;
-        return(
+        const scale = this.props.scale;
+
+        return (
             <fieldset>
-                <legend>Enter temperature in Celsius:</legend>
-                <input value={temperature}
-                       onChange={this.handleChange} />
-                <BoilingVerdict 
-                    celsius={parseFloat(temperature)}
-                />
+                <legend>Enter temperature in {scaleNames[scale]}:</legend>
+                <input value={temperature} onChange={this.handleChange} />
             </fieldset>
+        )
+    }
+}
+
+class Calculator extends React.Component {
+    render() {
+        return (
+            <div>
+                < TemperatureInput scale="c" />
+                < TemperatureInput scale="f" />
+            </div>
         )
     }
 }
